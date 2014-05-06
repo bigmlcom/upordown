40804a76e6cae21aca96bcd2b1cf2a935c331801
Oscar Rovira <rovira@bigml.com>
2014-05-06 19:00:42 +0200

    Display prediction result and error handling

--- app/js/app.js
+++ app/js/app.js
@@ -46,23 +46,61 @@ $(function() {
     normalizeYesterdayClose();
     normalizeOpen();
 
-    var $predictionFrom = $('form');
+    var $predictionFrom = $('form'),
+        $button = $('button', $predictionFrom),
+        $result = $('#result'),
+        $resultError = $('.alert', $result),
+        $resultArrow = $('.arrow', $result),
+        $resultConfidence = $('.confidence strong', $result);
 
+    // Reset result element on form inputs change
+    $('input', $predictionFrom).on('change', function() {
+        $resultError.html(null).hide();
+
+        $resultArrow.removeClass('up down');
+        $resultConfidence.html('?');
+    });
+
+    // Prevents from multiple form submit
+    $button.on('click', function(e) {
+        e.preventDefault();
+
+        if ($button.hasClass('disabled')) {
+            return false;
+        }
+        $button.addClass('disabled');
+
+        $predictionFrom.trigger('submit');
+    });
+
+
+    // Submit the prediction form
     $predictionFrom.on('submit', function(e) {
         e.preventDefault();
 
         normalizeYesterdayClose();
         normalizeOpen();
 
+        /*
+         * Prepare input_data dictionary:
+         * { field_id_1 : value_1, .., field_id_n : value_n }
+         */
         var inputData = {};
         $('.input-data', $predictionFrom).each(function() {
             var $this = $(this);
             inputData[$this.attr('name')] = $this.val();
         });
 
+        $resultError.html(null).hide();
+        $resultArrow.removeClass('up down');
+        $resultConfidence.html('?');
+        $button.addClass('loading');
+
+        // https://bigml.com/developers/predictions#p_create
         var postData = {
             model: modelResource,
-            input_data: inputData
+            input_data: inputData,
+            tags: ['BigMLWorkshop'] // Optional (just for filter predictions)
         };
 
         console.log("Input data: " + JSON.stringify(inputData));
@@ -74,9 +112,44 @@ $(function() {
             dataType: 'json',
             data: JSON.stringify(postData),
             success: function(prediction) {
+
+                $button.removeClass('disabled loading');
+
+                if (prediction.status.code == FINISHED) {
+                    // Prediction created successfully. Render result
+                    $resultArrow.addClass(prediction.output.toLowerCase());
+
+                    $resultConfidence.html(
+                        (prediction.confidence * 100).toFixed(2) + '%'
+                    );
+                } else {
+                    // An error happended creating the prediction.
+                    $resultError.html(
+                        prediction.status.message
+                    ).show();
+
+                    $resultArrow.addClass('up down');
+                    $resultConfidence.html('!');
+                }
+
                 console.log(prediction);
             },
             error: function(error) {
+                // An error happended creating the prediction.
+                $button.removeClass('disabled loading');
+
+                var message = 'Something went wrong creating the prediction.';
+                if (error.responseJSON &&
+                    error.responseJSON.status &&
+                    error.responseJSON.status.message) {
+
+                    message = error.responseJSON.status.message;
+                }
+                $resultError.html( message ).show();
+
+                $resultArrow.addClass('up down');
+                $resultConfidence.html('!');
+
                 console.log(error);
             }
 
